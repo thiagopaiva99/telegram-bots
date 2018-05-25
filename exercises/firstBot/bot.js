77de12f9cc96567857160348730f6b78f03824d4
Thiago Galvani <thiagopaiva99@gmail.com>
2018-05-25 15:00:40 -0300

    Initing the bot start middleware

--- exercises/firstBot/bot.js
+++ exercises/firstBot/bot.js
@@ -1,3 +1,12 @@
 const env = require('../../.env')
 const Telegraf = require('telegraf')
 const bot = new Telegraf(env.token)
+
+bot.start(ctx => {
+    const from = ctx.update.message.from
+
+    console.log(from)
+    ctx.reply(`Fala ae ${from.first_name}`)
+})
+
+bot.startPolling()
