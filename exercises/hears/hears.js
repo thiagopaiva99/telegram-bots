c7a79380399e07a66a347728f14036f606b2467d
Thiago Galvani <thiagopaiva99@gmail.com>
2018-05-26 23:23:49 -0300

    Adding the first word hear

--- exercises/hears/hears.js
+++ exercises/hears/hears.js
@@ -2,3 +2,10 @@ const env = require('../../.env')
 const Telegraf = require('telegraf')
 const bot = new Telegraf(env.token)
 const utils = require('../utils.js')
+const moment = require('moment')
+
+bot.hears('pizza', ctx => {
+    const user = utils.getUser(ctx)
+
+    ctx.reply(`${user.first_name}, você falou pizza? QUERO!`)
+})
