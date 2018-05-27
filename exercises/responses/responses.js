6397d4bcfaa592a0dda15d694eefd383f92bfc46
Thiago Galvani <thiagopaiva99@gmail.com>
2018-05-26 21:55:19 -0300

    Chceking the reply types

--- exercises/responses/responses.js
+++ exercises/responses/responses.js
@@ -6,6 +6,8 @@ const utils = require('../utils.js')
 bot.start(async ctx => {
     const user = utils.getUser(ctx)
     await ctx.reply(`Seja bem vindo ${user.first_name}!`)
+    await ctx.replyWithHTML(`Resposta com <i>HTML</i>`)
+    await ctx.replyWithMarkdown(`Resposta com *Markdown*`)
 })
 
 bot.startPolling()
