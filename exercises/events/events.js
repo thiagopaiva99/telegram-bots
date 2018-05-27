c28398c884b746f44eecd491e98ed3fe98a73cf0
Thiago Galvani <thiagopaiva99@gmail.com>
2018-05-26 21:28:17 -0300

    Setting up the start event

--- exercises/events/events.js
+++ exercises/events/events.js
@@ -1,3 +1,11 @@
 const env = require('../../.env')
 const Telegraf = require('telegraf')
 const bot = new Telegraf(env.token)
+
+bot.start(ctx => {
+    const user = getUser(ctx)
+
+    ctx.reply(`Seja bem-vindo ${user.first_name}`)
+})
+
+const getUser = ctx => ctx.update.message.from
