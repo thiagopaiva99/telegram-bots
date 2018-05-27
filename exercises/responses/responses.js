24be0403561a6246ea039128ba69458c4e6ea58f
Thiago Galvani <thiagopaiva99@gmail.com>
2018-05-26 21:51:49 -0300

    Start with the 'start' event

--- exercises/responses/responses.js
+++ exercises/responses/responses.js
@@ -2,3 +2,8 @@ const env = require('../../.env')
 const Telegraf = require('telegraf')
 const bot = new Telegraf(env.token)
 const utils = require('../utils.js')
+
+bot.start(async ctx => {
+    const user = utils.getUser(ctx)
+    await ctx.reply(`Seja bem vindo ${user.first_name}!`)
+})
