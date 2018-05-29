34a61ef8f7b08bcb60331c90b1dec8a12289c195
Thiago Galvani <thiagopaiva99@gmail.com>
2018-05-29 12:45:58 -0300

    Create the list and buttons

--- projects/shoppingListV1/list.js
+++ projects/shoppingListV1/list.js
@@ -1,4 +1,17 @@
 const env = require('../../.env')
 const Telegraf = require('telegraf')
+const Extra = require('telegraf/extre')
+const Markup = require('telegraf/markup')
 const bot = new Telegraf(env.token)
 const utils = require('../utils.js')
+
+let list = []
+
+const buttons = () => Extra.markup(
+    Markup.inlineKeyboard(
+        list.map(item => Markup.callbackButton(item, `delete ${item}`)),
+        {
+            columns: 3
+        }
+    )
+)
