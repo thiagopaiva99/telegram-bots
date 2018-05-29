e093f9ccf42f69f33ff2adeb282206d6fdffe7f4
Thiago Galvani <thiagopaiva99@gmail.com>
2018-05-29 10:16:45 -0300

    Config the start

--- exercises/commands/command.js
+++ exercises/commands/command.js
@@ -2,3 +2,7 @@ const env = require('../../.env')
 const Telegraf = require('telegraf')
 const bot = new Telegraf(env.token)
 const utils = require('../utils.js')
+
+bot.start(ctx => {
+    const user = utils.getUser(ctx)
+})
