7d4f66901f8a7bddccbaca1fb3988f5d5f078b5e
Thiago Galvani <thiagopaiva99@gmail.com>
2018-05-29 21:33:21 -0300

    Creating the shopppingList V2

---
+++ projects/shoppingListV2/list.js
@@ -0,0 +1,40 @@
+const env = require('../../.env')
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+const bot = new Telegraf(env.token)
+const utils = require('../../exercises/utils')
+
+let list = []
+
+const buttons = () => Extra.markup(
+    Markup.inlineKeyboard(
+        list.map(item => Markup.callbackButton(item, `delete ${item}`)),
+        {
+            columns: 3
+        }
+    )
+)
+
+bot.start(async ctx => {
+    const user = utils.getUser(ctx)
+
+    await ctx.reply(`Seja bem-vindo ${user.first_name}`)
+    await ctx.reply(`Digite os itens que você deseja adicionar...`)
+})
+
+bot.on('text', ctx => {
+    const item = ctx.update.message.text
+
+    list.push(item)
+    ctx.reply(`${item} adicionado!`, buttons())
+})
+
+bot.action(/delete (.+)/, ctx => {
+    const item = ctx.match[1]
+
+    list = list.filter(listItem => listItem !== item)
+    ctx.reply(`${item} deletado!`, buttons())
+})
+
+bot.startPolling()
