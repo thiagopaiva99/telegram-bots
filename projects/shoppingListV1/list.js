047b334f08021462492afee6da87d2ba4f66ad77
Thiago Galvani <thiagopaiva99@gmail.com>
2018-05-29 21:40:39 -0300

    Finishing the shoppingList per user

--- projects/shoppingListV1/list.js
+++ projects/shoppingListV1/list.js
@@ -2,10 +2,11 @@ const env = require('../../.env')
 const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
+const session = require('telegraf/session')
 const bot = new Telegraf(env.token)
 const utils = require('../../exercises/utils')
 
-const buttons = () => Extra.markup(
+const buttons = (list) => Extra.markup(
     Markup.inlineKeyboard(
         list.map(item => Markup.callbackButton(item, `delete ${item}`)),
         {
@@ -29,14 +30,14 @@ bot.on('text', ctx => {
     const item = ctx.update.message.text
 
     ctx.session.list.push(item)
-    ctx.reply(`${item} adicionado!`, buttons())
+    ctx.reply(`${item} adicionado!`, buttons(ctx.session.list))
 })
 
 bot.action(/delete (.+)/, ctx => {
     const item = ctx.match[1]
 
     ctx.session.list = ctx.session.list.filter(listItem => listItem !== item)
-    ctx.reply(`${item} deletado!`, buttons())
+    ctx.reply(`${item} deletado!`, buttons(ctx.session.list))
 })
 
 bot.startPolling()
